1b3a705fd4eb8704ee8bbf83ea2f7b99de62dfbf
kodmanspp <k.bayaman@bk.ru>
2021-11-02 02:37:58 +0600

    end homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+
 import './App.css';
+import ListTimers from './components/ListTimers';
+import NewTimer from './components/NewTimer';
+import { updateTimer } from './store/actions/timer';
+import store from './store/config';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let lastUpdate =  Date.now();
+    setInterval(()=>{
+        const now =  Date.now();
+        const diff = now - lastUpdate;
+        lastUpdate = now;  
+        store.dispatch(updateTimer(diff));
+    }, 100);
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <NewTimer/>
+                <ListTimers/>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
